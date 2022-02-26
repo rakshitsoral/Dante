df7454650c0ac5a4d74d658234c91d4a210761c3
Rakshit Soral <rakshitsoral@gmail.com>
2022-02-26 16:50:05 +0530

    Added post

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Ted Goas',
-  description: 'Ted Goas is a reliable product designer and front-end developer working on websites, applications, and HTML emails.',
+  title: 'Rakshit Soral',
+  description: 'Rakshit is a reliable Digital Marketer, blogger, and a hobbyist developer playing around with HTML, CSS, wordpress and JAMstack.',
   // keywords: ['eleventy', 'template', 'simple', 'clean'],
   // your site url without trailing slash
-  url: 'https://www.tedgoas.com',
+  url: 'https://rakshitsoral.com',
   // how many posts you want to show for each page
   // paginate: 6
 };
