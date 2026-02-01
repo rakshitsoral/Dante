0488177bff30be070c58bb6827b659025ce84e6a
Rakshit Soral <40488334+rakshitsoral@users.noreply.github.com>
2026-02-01 13:42:16 +0530

    Made changes in blog and hero background svg

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,52 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addPassthroughCopy('src/assets/img/');
-  eleventyConfig.addPassthroughCopy({ 'src/posts/img/': 'assets/img/' });
-  eleventyConfig.addPassthroughCopy({ 'src/work/img/': 'assets/img/' });
-  eleventyConfig.addPassthroughCopy('src/assets/files/');
+  // Passthrough
+  eleventyConfig.addPassthroughCopy("src/assets/img/");
+  eleventyConfig.addPassthroughCopy({ "src/posts/img/": "assets/img/" });
+  eleventyConfig.addPassthroughCopy({ "src/work/img/": "assets/img/" });
+  eleventyConfig.addPassthroughCopy("src/assets/files/");
+  eleventyConfig.addPassthroughCopy("src/admin");
 
+  // Watch targets
   eleventyConfig.addWatchTarget("src/assets/js/");
   eleventyConfig.addWatchTarget("src/assets/css/");
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-  eleventyConfig.addLayoutAlias('work', 'layouts/work.njk');
+  // Layout aliases
+  eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("work", "layouts/work.njk");
 
-  eleventyConfig.addFilter('readableDate', require('./lib/filters/readableDate'));
-  eleventyConfig.addFilter('minifyJs', require('./lib/filters/minifyJs'));
+  // Filters
+  eleventyConfig.addFilter("readableDate", require("./lib/filters/readableDate"));
+  eleventyConfig.addFilter("minifyJs", require("./lib/filters/minifyJs"));
 
-  eleventyConfig.addTransform('postcss', require('./lib/transforms/postcss'));
-  eleventyConfig.addTransform('minifyHtml', require('./lib/transforms/minifyHtml'));
+  // ✅ FIX: htmlDateString filter (NOW LOADED)
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return new Date(dateObj).toISOString().split("T")[0];
+  });
 
-  eleventyConfig.addCollection('posts', require('./lib/collections/posts'));
-  eleventyConfig.addCollection('work', require('./lib/collections/work'));
-  eleventyConfig.addCollection('pagedPosts', require('./lib/collections/pagedPosts'));
-  eleventyConfig.addPassthroughCopy("src/admin");
+  // Transforms
+  eleventyConfig.addTransform("postcss", require("./lib/transforms/postcss"));
+  eleventyConfig.addTransform("minifyHtml", require("./lib/transforms/minifyHtml"));
 
+  // Collections
+  eleventyConfig.addCollection("posts", require("./lib/collections/posts"));
+  eleventyConfig.addCollection("work", require("./lib/collections/work"));
+  eleventyConfig.addCollection("pagedPosts", require("./lib/collections/pagedPosts"));
+
+  // Plugins
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist",
     },
-    // pathPrefix: "/subfolder/",
-    templateFormats: ['md', 'njk', 'html'],
-    dataTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk'
+    templateFormats: ["md", "njk", "html"],
+    dataTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
 };
